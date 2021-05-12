661d46beb98c8c727cf96c551673573755dafeab
Omar Ramadan <ramomar1992@outlook.com>
2021-05-12 18:21:11 +0300

    Linked Lists

---
+++ javascript/linked-lists/__tests__/linked-lists.test.js
@@ -0,0 +1,55 @@
+const {Node , LinkedList} = require('../linked-lists');
+
+describe('Testing creating a node', () => {
+    it('should create a node with null next', () => {
+        let node = new Node();
+        expect(node).toBeDefined();
+        expect(node.next).toBe(null);
+        expect(node.data).toBe(undefined);
+    });
+    it('should create a node with defined data', () => {
+        let newNode = new Node('new');
+        expect(newNode.data).toEqual('new');
+    });
+});
+describe('linked lists', () => {
+    it('should return an empty linked list', () => {
+        const myList = new LinkedList();
+        expect(myList.head).toBe(null)
+    });
+    it('should insert into the linked list', () => {
+        let myList = new LinkedList();
+        myList.insert('Hello World');
+        expect(myList.head).toBeTruthy();
+    });
+    it('Can properly insert multiple nodes into the linked list', () => {
+        let myList = new LinkedList();
+        myList.insert('Hello');
+        myList.insert('world');
+        expect(myList.head.data).toEqual('world')
+        expect(myList.head.next.data).toBe('Hello');
+    });
+    it('Will return true when finding a value within the linked list that exists', () => {
+        let myList = new LinkedList();
+        myList.insert('Hello');
+        myList.insert('world');
+        myList.insert('Hello');
+        expect(myList.includes('Hello')).toBeTruthy();
+        expect(myList.includes('world')).toBeTruthy();
+    });
+    it('Will return false when searching for a value in the linked list that does not exist', () => {
+        let myList = new LinkedList();
+        myList.insert('Hello');
+        myList.insert('world');
+        myList.insert('Hello');
+        expect(myList.includes('hi')).toBe(false);
+        expect(myList.includes('My name')).toBe(false);
+    });
+    it('Can properly return a collection of all the values that exist in the linked list', () => {
+        let myList = new LinkedList();
+        myList.insert('Hello');
+        myList.insert('world');
+        expect(myList.toString()).toBe('{world} -> {Hello} -> NULL');
+    });
+
+});
---
+++ javascript/linked-lists/linked-lists.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList{
+  constructor() {
+    this.head = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.head != null) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+    }
+    return this;
+  }
+  includes(val) {
+    let current = this.head;
+    while (current) {
+      if (current.data == val) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  toString() {
+    let resultingString = '';
+    let current = this.head;
+    if (!current) {
+      return 'NULL';
+    }
+    while (current) {
+      resultingString += `{${current.data}} -> `;
+      current = current.next;
+    }
+    return resultingString + 'NULL';
+  }
+}
+
+module.exports = {
+  Node,LinkedList
+}
